6aba69bd594fe00ee90fe7e6b402d11cd8413731
Rasel Mahmud <raselm606@gmail.com>
2026-02-09 03:55:48 +0600

    text added

--- components/WhyYou.jsx
+++ components/WhyYou.jsx
@@ -1,16 +1,32 @@
 import Image from "next/image"
 import Link from "next/link"
 import card_woman from "../public/images/card_woman.png"
+import lines from "../public/images/lines.png"
+import lines_two from "../public/images/lines_2.png"
 import star from "../public/images/star.png"
 const WhyYou = () => {
   return (
     <>
     <div className="max-w-7xl bg-[#DF1C25] mx-auto px-12 pt-15 relative rounded-2xl  pb-10 mb-20">
+        
         <Image className="
         md:absolute  
         right-0
         -top-22
-        " src={card_woman} alt="" />
+        z-1
+        " src={card_woman} alt="#" />
+        
+        <Image className="
+        md:absolute   
+        bottom-0 left-1/2 -translate-x-1/2 
+        " src={lines} alt="#" />
+        
+        <Image className="
+        md:absolute  
+        -bottom-4
+        right-[25%]
+        " src={lines_two} alt="#" />
+
         <div className="grid grid-cols-12">
             <div className="md:col-span-6 col-span-12">
                 <h2 className="
@@ -48,6 +64,10 @@ const WhyYou = () => {
                 " href="#">Buy Now</Link>
             </div>
         </div>
+        
+    </div>
+    <div className="max-w-7xl mx-auto mt-8 mb-15">
+        <p className="text-center md:w-[70%] text-[22px] text-[#696984] mx-auto">Onlearing  is one powerful online software suite that combines all the tools needed to run a successful school or office.</p>
     </div>
     </>
   )
